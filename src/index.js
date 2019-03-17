c7e93639833a26f646c342a64df9faf8ef5dc067
KrDior <xzx89s@gmail.com>
2019-03-17 23:55:36 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let n = number;
+  let k = base;
+  let test = 1e20;
+  for (let i = 2; i <= k; i++) {
+    if (k % i == 0) {
+      let cnt_k1 = 0;
+      while (k % i == 0) {
+        cnt_k1++;
+        k /= i;
+      }
+      //console.log('cnt_k1', cnt_k1)
+      let cnt_k2 = 0;
+      let t = n;
+      while (t / i > 0 && t > i) {
+        cnt_k2 += Math.floor(t/i);
+        t /= i;
+      }
+      
+      test = Math.min(test, cnt_k2/cnt_k1);
+      test = Math.floor(test);
+    }
+
+  }
+  
+  return (test > 0) ? test : 1;
 }
